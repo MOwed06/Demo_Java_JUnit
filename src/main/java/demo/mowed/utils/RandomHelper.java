95afa6d16cda2826fe58dd666768af2f7905a9ba
MOwed <michael.owed@gmail.com>
2026-01-12 15:58:35 -0500

    fix ApplicationRunner test, add RandomHelper

---
+++ src/main/java/demo/mowed/utils/RandomHelper.java
@@ -0,0 +1,68 @@
+package demo.mowed.utils;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
+public class RandomHelper {
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    private static Random random = new Random();
+    private static List<String> firstNames;
+    private static List<String> lastNames;
+    private static List<String> sentences;
+
+    static {
+        try {
+            firstNames = readSetupDataFile("firstNames.json");
+            lastNames = readSetupDataFile("familyNames.json");
+            sentences = readSetupDataFile("poetryRFrost.json");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static List<String> readSetupDataFile(String fileName) throws IOException {
+        var fullName = String.format("data/%s", fileName);
+        return OBJECT_MAPPER.readValue(new File(fullName),
+                new TypeReference<List<String>>() {});
+    }
+
+    public static int getInt(int min, int max) {
+        return random.nextInt(min, max);
+    }
+
+    public static float getFloat(float min, float max) {
+        return random.nextFloat(min, max);
+    }
+
+    public static <T> T selectItem(List<T> source) {
+        int selectIndex = getInt(0, source.size());
+        return source.get(selectIndex);
+    }
+
+    public static String getPerson() {
+        var firstName = selectItem(firstNames);
+        var lastName = selectItem(lastNames);
+        return String.format("%s %s", firstName, lastName);
+    }
+
+    public static String getGUID() {
+        var guid = UUID.randomUUID();
+        return guid.toString().toUpperCase();
+    }
+
+    // demo purposes only
+    public static void main(String[] args) {
+        try {
+            var person = RandomHelper.getPerson();
+            System.out.println(person);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
---
+++ src/test/java/demo/mowed/services/AccountServiceTest.java
@@ -0,0 +1,16 @@
+package demo.mowed.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountServiceTest {
+
+    @Test
+    void getAccount() {
+    }
+
+    @Test
+    void addAccount() {
+    }
+}
